82e0730a4eb4daac33d03bd7881937468e317d2d
Dawud Rehan <d.rehan7@gmail.com>
2021-01-27 16:31:56 -0500

    got everything working; gonna try to implement bonus steps

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const equalButton = document.querySelector("#equal-sign");
 const textArea = document.querySelector(".text-box");
 const clearButton = document.querySelector("#clear");
-const button = document.querySelectorAll(".button");
+const buttons = document.querySelectorAll(".button");
 
 
 function insertNumber(number) {
     // textArea.value += number;
-    if (textArea.value === '0') {
-        textArea.value = number;
+    if (textArea.value === '' && number === ".") {
+        textArea.value = "0" + number;
     } else {
         textArea.value += number;
     }
@@ -15,21 +15,31 @@ function insertNumber(number) {
 
 
 function calculate(operation) {
-    let answer = eval(operation);
-    textArea.value = answer;
+    try {
+        let answer = eval(operation);
+        textArea.value = answer;
+    }
+    catch (e) {
+        clearField();
+    }
 }
 
 
 function clearField() {
-    textArea.value = 0;
+    textArea.value = "";
 }
 
 
 
+for (let button of buttons) {
+    button.addEventListener("click", e => {
+        let number = e.target.value;
+        insertNumber(number);
+    })
+}
 
 equalButton.addEventListener("click", e => {
     let operation = textArea.value;
-    console.log(operation);
     calculate(operation);
 })
 
