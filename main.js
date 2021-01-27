6bb093a187afe43a95cf2b537fbebf5d6322f824
Dawud Rehan <d.rehan7@gmail.com>
2021-01-27 16:19:56 -0500

    checkpoint; about to do some js so in case i mess everything up i have this

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-document.body.style.backgroundColor = "grey";
+const equalButton = document.querySelector("#equal-sign");
+const textArea = document.querySelector(".text-box");
+const clearButton = document.querySelector("#clear");
+const button = document.querySelectorAll(".button");
+
+
+function insertNumber(number) {
+    // textArea.value += number;
+    if (textArea.value === '0') {
+        textArea.value = number;
+    } else {
+        textArea.value += number;
+    }
+}
+
+
+function calculate(operation) {
+    let answer = eval(operation);
+    textArea.value = answer;
+}
+
+
+function clearField() {
+    textArea.value = 0;
+}
+
+
+
+
+equalButton.addEventListener("click", e => {
+    let operation = textArea.value;
+    console.log(operation);
+    calculate(operation);
+})
+
+clearButton.addEventListener("click", e => {
+    clearField();
+})
+
